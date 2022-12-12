5a4fc1f22cf0c1b3de180bdffdf6997388c862a9
Mauro Ladeia Filho <70155645+mauroLf@users.noreply.github.com>
2022-12-12 17:04:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    $classe = $_GET['class'];
+    $metodo = $_GET['acao'];
+    
+    $classe .= 'Controller';
+    
+    require_once 'controller/'.$classe.'.php';
+    
+    $obj = new $classe();
+    $obj->$metodo();
+?>
